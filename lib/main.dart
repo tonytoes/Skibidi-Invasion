4b930c6180f0fcead9bb7783ef44f287b8541c17
tonytoes <anthonymiguel2005@gmail.com>
2025-04-13 03:33:13 +0800

    Home Screen Progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'scenario.dart'; // make sure this import is correct!
 
 void main() {
   runApp(const MyApp());
@@ -10,15 +11,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(
+      home: const HomeScreen(), // pull the screen into its own widget
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
         alignment: Alignment.center,
         padding: const EdgeInsets.all(32),
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage(
-              'images/background/figmadraft_homescreen.png',
+              'assets/images/bg/titlescreen_bg.png',
             ),
             fit: BoxFit.cover,
+          ),
+        ),
+        child: TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ScenarioScreen(),
+              ),
+            );
+          },
+          child: Text(
+            'Play',
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+            ),
           ),
         ),
       ),
---
+++ lib/scenario.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ScenarioScreen extends StatelessWidget {
+  const ScenarioScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(32),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bg/ruins_bg.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
