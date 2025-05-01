4a864fc23610533b37c07e3230c063c2f58b3c9d
Im Sauce <130982799+ImSauce@users.noreply.github.com>
2025-05-01 12:14:55 +0800

    2.0.7 update bg

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,6 +9,7 @@ android {
     namespace = "com.example.skibidi_invasion"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
+    
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
