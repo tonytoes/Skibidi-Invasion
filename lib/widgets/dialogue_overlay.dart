d9c3ae57fa84f724ce4290bbda6831fe830a8e69
Im Sauce <130982799+ImSauce@users.noreply.github.com>
2025-05-06 22:11:41 +0800

    2.11.3 fixed quick dialoge complete tap

--- lib/widgets/dialogue_overlay.dart
+++ lib/widgets/dialogue_overlay.dart
@@ -30,8 +30,7 @@ class _DialogueBoxWidgetState extends State<DialogueBoxWidget> {
   @override
   void initState() {
     super.initState();
-    _tapGestureRecognizer = TapGestureRecognizer()
-    ..onTap = _tap;
+    _tapGestureRecognizer = TapGestureRecognizer()..onTap = _tap;
     _startTypewriter();
   }
 
@@ -45,14 +44,13 @@ class _DialogueBoxWidgetState extends State<DialogueBoxWidget> {
   }
 
   Future<void> _startTypewriter() async {
-
     _isTyping = false;
     await Future.delayed(const Duration(milliseconds: 50));
 
-    if(!mounted) return;
+    if (!mounted) return;
 
     setState(() {
-      _visibleText = '';  // Clearsssss the previous text
+      _visibleText = ''; // Clearsssss the previous text
     });
     _isTyping = true;
 
@@ -71,17 +69,30 @@ class _DialogueBoxWidgetState extends State<DialogueBoxWidget> {
     _isTyping = false; // Finished typing
   }
 
-  void _tap() {
-    if (_isTyping) {
-      setState(() {
-        _visibleText = widget.dialogueText;
-        _isTyping = false;
-      });
-    } else {
-      widget.nextDialogue(null);
-    }
+  Future<void> noTypewriter() async {
+    _isTyping = false;
+    await Future.delayed(const Duration(milliseconds: 50));
+
+    if (!mounted) return;
+
+    setState(() {
+      _visibleText = widget.dialogueText;
+    });
   }
 
+  void _tap() {
+    if (widget.hasChoices == false) {
+      if (_isTyping) {
+        setState(() {
+          _visibleText = widget.dialogueText;
+          _isTyping = false;
+          noTypewriter();
+        });
+      } else {
+        widget.nextDialogue(null);
+      }
+    } 
+  }
 
   @override
   void dispose() {
@@ -99,67 +110,61 @@ class _DialogueBoxWidgetState extends State<DialogueBoxWidget> {
         child: GestureDetector(
           behavior: HitTestBehavior.translucent,
           onTap: _tap,
-        child: Stack(
-          children: [
-            Image.asset(
-              'assets/images/dialogue/dialogue-box.png', // I changed the dialogue box image here with no opacity
-              fit: BoxFit.fill,
-              width: double.infinity,
-              height: 220,
-            ),
-            Positioned(
-              left: 30,
-              top: 25,
-              right: 30,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  if (widget.characterName.trim().isNotEmpty) ...[
+          child: Stack(
+            children: [
+              Image.asset(
+                'assets/images/dialogue/dialogue-box.png', // I changed the dialogue box image here with no opacity
+                fit: BoxFit.fill,
+                width: double.infinity,
+                height: 220,
+              ),
+              Positioned(
+                left: 30,
+                top: 25,
+                right: 30,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if (widget.characterName.trim().isNotEmpty) ...[
+                      Text(
+                        widget.characterName,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'IBMPlexMono',
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 1),
+                      Container(height: 1, color: Colors.black),
+                      const SizedBox(height: 5),
+                    ],
                     Text(
-                      widget.characterName,
+                      _visibleText,
                       style: const TextStyle(
                         fontSize: 20,
                         fontFamily: 'IBMPlexMono',
                         color: Colors.black,
-                        fontWeight: FontWeight.bold,
+                        height: 1,
                       ),
                     ),
-                    const SizedBox(height: 1),
-                    Container(
-                      height: 1,
-                      color: Colors.black,
-                    ),
-                    const SizedBox(height: 5),
                   ],
-                  Text(
-                    _visibleText,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontFamily: 'IBMPlexMono',
-                      color: Colors.black,
-                      height: 1,
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-            Positioned(
-              right: 3,
-              bottom: 5,
-              child: Row(
-                children: [
-                  Visibility(
-                    visible: !widget.hasChoices && !_isTyping,
-                    child:
-                    IconButton(
-                      icon: Image.asset(
-                        'assets/icons/nextBT.png'
-                      ),
-                      onPressed: () => widget.nextDialogue(null),
+              Positioned(
+                right: 3,
+                bottom: 5,
+                child: Row(
+                  children: [
+                    Visibility(
+                      visible: !widget.hasChoices && !_isTyping,
+                      child: IconButton(
+                        icon: Image.asset('assets/icons/nextBT.png'),
+                        onPressed: () => widget.nextDialogue(null),
                         iconSize: 30,
                       ),
                     ),
-                  ]
+                  ],
                 ),
               ),
             ],
